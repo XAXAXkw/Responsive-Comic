4c31b0fa01caf3685d863f4d6ad5a8a4d0e5cee7
Ivan Cuadros <ivancuadros@gmail.com>
2023-01-25 21:37:53 +0100

    0

---
+++ comic/src/components/NavBar.vue
@@ -0,0 +1,42 @@
+<template>
+    <div id="NavBox">
+<div class="container">
+    <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'NavBar',
+        methods: {
+        someMethod(event) {
+            // clientX/Y gives the coordinates relative to the viewport in CSS pixels.
+            console.log(event.clientX);
+            console.log(event.clientY);
+
+            // pageX/Y gives the coordinates relative to the <html> element in CSS pixels.
+            console.log(event.pageX);
+            console.log(event.pageY);
+
+            // screenX/Y gives the coordinates relative to the screen in device pixels.
+            console.log(event.screenX);
+            console.log(event.screenY);
+        }
+    }
+    }
+</script>
+
+<style lang="scss" scoped>
+#NavBox{
+    background-color: aquamarine;
+    margin: 0 auto;
+
+
+    display:flex;
+    width:100%;
+}
+
+</style>
